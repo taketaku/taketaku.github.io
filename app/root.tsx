b5181b8d6bc8cc52348f9c6b7b0dbe6c5273ebd6
taketaku <vbvbsp48@gmail.com>
2022-05-29 15:31:40 +0900

    add tailwindcss

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +8,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
