698885c5c5d31e6e452e15556658157f55056f42
taketaku <vbvbsp48@gmail.com>
2022-05-29 20:10:21 +0900

    refa

--- app/components/routes/Index/MyFavorites/index.tsx
+++ app/components/routes/Index/MyFavorites/index.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 
 import { TechItem } from "@/components/routes/Index/MyFavorites/TechItem";
+import { Section } from "@/components/shared/Section";
 
 import { items } from "./data";
 
@@ -10,31 +11,21 @@ type Props = {
 
 export const MyFavorites: FC<Props> = (props) => {
   return (
-    <>
-      <div className="container xl:max-w-6xl mx-auto px-4">
-        <header className="text-center mx-auto mb-12 lg:px-20">
-          <h2 className="text-2xl leading-normal mb-2 font-bold text-white">
-            Technologies / Frameworks
-          </h2>
-          <p className="text-white leading-relaxed font-light text-xl mx-auto pb-2">
-            My favorite technologies!
-          </p>
-        </header>
+    <Section
+      title="Technologies / Frameworks"
+      subTitle="My favorite technologies!"
+    >
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {items.map((item) => {
+          return (
+            <TechItem
+              key={item.title}
+              title={item.title}
+              percentage={item.percentage}
+            />
+          );
+        })}
       </div>
-
-      <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {items.map((item) => {
-            return (
-              <TechItem
-                key={item.title}
-                title={item.title}
-                percentage={item.percentage}
-              />
-            );
-          })}
-        </div>
-      </section>
-    </>
+    </Section>
   );
 };
---
+++ app/components/shared/Section.tsx
@@ -0,0 +1,35 @@
+import type { FC, ReactNode } from "react";
+import tw from "tailwind-styled-components";
+
+type Props = {
+  title: string;
+  subTitle?: string;
+  children: ReactNode;
+};
+
+export const Section: FC<Props> = ({ title, subTitle, children }) => {
+  return (
+    <Wrapper>
+      <div className="container xl:max-w-6xl mx-auto px-4">
+        <header className="text-center mx-auto mb-12 lg:px-20">
+          <h2 className="text-2xl leading-normal mb-2 font-bold text-white">
+            {title}
+          </h2>
+          {subTitle && (
+            <p className="text-white leading-relaxed font-light text-xl mx-auto pb-2">
+              {subTitle}
+            </p>
+          )}
+        </header>
+      </div>
+
+      <section className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-4 py-12">
+        {children}
+      </section>
+    </Wrapper>
+  );
+};
+
+const Wrapper = tw.div`
+	mb-5
+`;
