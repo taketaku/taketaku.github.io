2e88cd6cd84f3e7e1f1d4f8c703e9cbf2feb3cb5
taketaku <vbvbsp48@gmail.com>
2022-05-30 11:57:48 +0900

    change site title

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "taketaku profile",
   viewport: "width=device-width,initial-scale=1",
 });
 
